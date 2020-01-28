04e268b49192935e5fcce22687ba4794a441d4e0
abigailchester <58968006+abigailchester@users.noreply.github.com>
2020-01-28 16:30:45 +0200

    Alphabet final version

--- week_3/day_3/alphabet/alphabet.js
+++ week_3/day_3/alphabet/alphabet.js
@@ -1,4 +1,8 @@
 const root = document.getElementById("root")
+root.style.display = "flex"
+root.style.width = "100%"
+root.style.alignItems = "flex-start"
+
 
 const alphabetlower = "abcdefghijklmnopqrstuvwxyz"
 
@@ -8,55 +12,30 @@ const array = alphabet.split("")
 
 console.log(array)
 
+
 for (var i = 0; i < array.length; i++) {
 	var box = document.createElement("div")
 	box.setAttribute("class", "box")
 	box.innerText = array[i]
-	root.appendChild(box)
 	box.style.border = "2px solid black";
 	box.style.height = "50px";
 	box.style.width = "50px";
 	box.style.display = "flex";
 	box.style.alignItems = "center";
 	box.style.justifyContent = "center";
-	// box.style.overflow = "hidden";
-	// box.style.position = "fixed"
-	// for (var item in box) {
-	// 	item.style.position: "absolute" + "50px"
-	// }
-	// box.style.position = "absolute";
-	// box.style.zIndex = "1000";
-	// box.style.flexDirection = "row";
 	box.setAttribute("draggable", "true");
+	root.appendChild(box)
+	
+
 	box.addEventListener("dragend", function(e){
+		console.log(e.target.innerHTML)
 		let x = e.clientX;
 		let y = e.clientY;
-		box.style.position = "absolute";
-		box.style.left = x + "px";
-		box.style.top = y + "px";
-		console.log(x, y)
+		e.target.style.left = x + "px";
+		e.target.style.top = y + "px";
+		e.target.style.position= "absolute"
+		console.log(x, e.target.style.left)
 
-		// box.onmousedown = function(event) {
-		// 	box.style.position = "absolute";
-		// 	moveAt(event.pageX, event.pageY)
-		// }
-		// function moveAt(pageX, pageY) {
-		// 	box.style.left = pageX - box.offsetWidth / 2 + "px";
-		// 	box.style.top = pageY - box.offsetHeight / 2 + "px";
-		// }
-		function onMouseMove(event){
-			moveAt(event.clientX, event.clientY);
-		}
-		// document.addEventListener("mousemove", onMouseMove);
-		// box.onmouseup = function() {
-		// 	document.removeEventListener("mousemove", onMouseMove);
-		// 	box.onmouseup = null;
-		// }
 	})
 
 }
-
-//STILL NEED TO DO: 
-// 1. align div boxes next to each other - position needs to be absolute except they can't be on top of each other
-// 2. make it so that the letter clicked is picked up, rather than Z
-// 3. make the position stay even when you pick up another box
