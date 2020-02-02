bac03c9eb2f5547242f2d999ecfed9aa54238f35
abigailchester <58968006+abigailchester@users.noreply.github.com>
2020-02-02 14:57:22 +0200

    Adds daily exercises

---
+++ week_4/day_1/exercises.py
@@ -0,0 +1,35 @@
+# Exercise 1
+
+print("Hello world \n" * 4)
+
+# Exercise 2
+
+print((99^3)*8)
+
+# Exercise 3
+
+name = "Abby"
+age = 25
+shoe_size = 40
+info = "Hi, my name is " + name + ". I am " + str(age) + " years old, but my birthday is February 3rd, so I will be " + str(age + 1) + " tonight at midnight. If you want to get me a present, my shoe size is " + str(shoe_size) + ". Cheers!"
+
+print(info)
+
+# Exercise 4
+
+my_age = 123879 + age
+
+print(my_age)
+
+# Exercise 5
+
+first_name = "Abby"
+last_name = "Chester"
+
+print(first_name + " " + last_name)
+
+# Exercise 6
+
+computer_brand = "Mac"
+
+print("I have a " + computer_brand + " computer.")
